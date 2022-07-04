821402ac9ce07f992db29578e8db2a543cb7139e
seubpong.mon <pjame.fb@gmail.com>
2022-07-04 20:04:16 +0700

    Added SysLog IP

--- 03-1-static-ip/gke-ingress-ip-1.tf
+++ 03-1-static-ip/gke-ingress-ip-1.tf
@@ -17,11 +17,10 @@ resource "google_dns_record_set" "gke_ingress_1_dns_wildcard" {
 }
 
 # SysLog service
-resource "google_compute_global_address" "gke_syslog_svc_1" {
+resource "google_compute_address" "gke_syslog_svc_1" {
   project      = var.project
   name         = "gke-syslog-svc-1"
-  ip_version   = "IPV4"
-  address_type = "EXTERNAL"
+  region       = "asia-southeast1"
 }
 
 resource "google_dns_record_set" "gke_syslog_svc_1_dns" {
@@ -32,5 +31,5 @@ resource "google_dns_record_set" "gke_syslog_svc_1_dns" {
   ttl  = 300
   managed_zone = "devops"
 
-  rrdatas = [google_compute_global_address.gke_syslog_svc_1.address]
+  rrdatas = [google_compute_address.gke_syslog_svc_1.address]
 }
